83dd6d4baa2020c689b4075a88e6c910d4ed1d12
rezza fauzi <rezzaf205@gmail.com>
2021-11-07 14:10:18 +0700

    menambahkan lab1.py

--- lab1.py
+++ lab1.py
@@ -5,16 +5,18 @@ uas = input("Masukkan nilai UAS:")
 tugas = input("Masukkan nilai Tugas:")
 akhir = (int(tugas) * .2) + (int(uts) * .4) + (int(uas) * .4)
 keterangan = ("TIDAK LULUS", "LULUS")[akhir > 60.0]
+
 if akhir > 80:
-huruf = "A"
+    huruf = "A"
 elif akhir > 70:
-huruf = "B"
+    huruf = "B"
 elif akhir > 50:
-huruf = "C"
+    huruf = "C"
 elif akhir > 40:
-huruf = "D"
+    huruf = "D"
 else:
-huruf = "E"
+    huruf = "E"
+
 print("\nNama :",nama)
 print("Nilai UTS :",uts)
 print("Nilai UAS :",uas)
