a6bdcc27caf7922f3c204f5d6dfa70328856ef21
rezza fauzi <rezzaf205@gmail.com>
2021-11-07 14:11:15 +0700

    menambahkan lab3.py

--- lab3.py
+++ lab3.py
@@ -2,7 +2,8 @@
 a = int(input("Masukkan bilangan A: "))
 b = int(input("Masukkan bilangan B: "))
 c = int(input("Masukkan bilangan C: "))
+
 if a+b == c or b+c == a or c+a == b:
-print("BENAR")
+    print("BENAR")
 else:
-print("SALAH")
+    print("SALAH")
